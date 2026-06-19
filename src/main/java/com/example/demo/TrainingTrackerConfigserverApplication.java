802e1ae206286b4aab67cae52388092f56d63e79
Yashpal Gawali <gawaliyashpal@gmail.com>
2026-06-19 11:21:41 +0530

    updated

--- src/main/java/com/example/demo/TrainingTrackerConfigserverApplication.java
+++ src/main/java/com/example/demo/TrainingTrackerConfigserverApplication.java
@@ -10,6 +10,6 @@ public class TrainingTrackerConfigserverApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TrainingTrackerConfigserverApplication.class, args);
+	
 	}
-
 }
